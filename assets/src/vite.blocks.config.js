63726a3b427691dde9e212583a7ea7bdb2977947
Henrique Cruz <eu@henriquecruz.com.br>
2025-10-08 21:41:18 -0300

    More build pipeline adjustments

--- assets/src/vite.blocks.config.js
+++ assets/src/vite.blocks.config.js
@@ -4,50 +4,94 @@ import wordPressAssetPlugin from './plugins/wordpress-asset';
 
 // Configurações de módulos externos do WordPress e WooCommerce
 const externalDeps = {
-    '@woocommerce/blocks-registry': 'wc.blocks.registry',
-    '@woocommerce/settings': 'wc.settings',
-    '@woocommerce/blocks-checkout': 'wc.blocks.checkout',
-    '@wordpress/element': 'wp.element',
-    '@wordpress/i18n': 'wp.i18n',
-    '@wordpress/html-entities': 'wp.htmlEntities',
-    '@wordpress/blocks': 'wp.blocks',
-    '@wordpress/block-editor': 'wp.blockEditor',
-    'jquery': 'jQuery',
-    'wp': 'wp'
+	'@woocommerce/blocks-registry'  : 'window.wc.wcBlocksRegistry',
+	'@woocommerce/settings'       	: 'window.wc.wcSettings',
+	'@woocommerce/blocks-checkout'  : 'window.wc.blocksCheckout',
+	'@wordpress/element'            : 'wp.element',
+	'@wordpress/i18n'               : 'wp.i18n',
+	'@wordpress/html-entities'      : 'wp.htmlEntities',
+	'@wordpress/blocks'             : 'wp.blocks',
+	'@wordpress/block-editor'       : 'wp.blockEditor',
+	'jquery'                        : 'jQuery',
+	'wp'                            : 'wp'
 };
 
 // Configuração específica para WooCommerce Blocks
 export default defineConfig(({ command, mode }) => {
-    const isProduction = mode === 'production';
-    const isDevelopment = mode === 'development';
+	const isProduction = mode === 'production';
+	const isDevelopment = mode === 'development';
 
-    return {
-        build: {
-            outDir: '../dist/js',
-            emptyOutDir: false,
-            minify: isProduction,
-            sourcemap: isDevelopment,
-            lib: {
-                entry: './js/blocks/woocommerce/index.jsx',
-                formats: ['iife'],
-                name: 'PaghiperBlocks',
-                fileName: () => 'blocks.min.js'
-            },
-            rollupOptions: {
-                external: Object.keys(externalDeps),
-                output: {
-                    globals: externalDeps
-                }
-            }
-        },
-        plugins: [
-            react({
-                include: [/\.(jsx|js)$/],
-                babel: {
-                    presets: ['@babel/preset-react']
-                }
-            }),
-            wordPressAssetPlugin()
-        ]
-    };
+	const commonConfig = {
+		optimizeDeps: {
+			exclude: Object.keys(externalDeps)
+		}
+	};
+
+	return {
+		...commonConfig,
+		build: isDevelopment ? {
+			outDir: '../dist/js',
+			emptyOutDir: false,
+			sourcemap: true,
+			minify: false,
+			rollupOptions: {
+				input: './js/blocks/woocommerce/index.jsx',
+				external: Object.keys(externalDeps),
+				output: {
+					format: 'es',
+					name: 'PaghiperBlocks',
+					globals: externalDeps,
+					entryFileNames: 'blocks.min.js'
+				}
+			}
+		} : {
+			outDir: '../dist/js',
+			emptyOutDir: false,
+			minify: true,
+			sourcemap: false,
+			lib: {
+				entry: './js/blocks/woocommerce/index.jsx',
+				formats: ['iife'],
+				name: 'PaghiperBlocks',
+				fileName: () => 'blocks.min.js'
+			},
+			rollupOptions: {
+				external: Object.keys(externalDeps),
+				output: {
+					globals: externalDeps
+				}
+			}
+		},
+		server: isDevelopment ? {
+			port: 5174,
+			base: '/blocks/',
+			host: 'wordpress.sandbox.local',
+			https: {
+				key: './certificates/wordpress.sandbox.local+3-key.pem',
+				cert: './certificates/wordpress.sandbox.local+3.pem'
+			},
+			watch: {
+				include: ['./js/blocks/**/*.{js,jsx}'],
+				exclude: ['node_modules/**', 'dist/**']
+			},
+			hmr: {
+				protocol: 'wss',
+				host: 'wordpress.sandbox.local',
+				clientPort: 5174,
+			},
+			cors: {
+				origin: '*',
+			},
+			middlewareMode: false
+		} : {},
+		plugins: [
+			react({
+				include: [/\.(jsx|js)$/],
+				babel: {
+					presets: ['@babel/preset-react']
+				}
+			}),
+			wordPressAssetPlugin()
+		]
+	};
 });
--- assets/src/vite.config.js
+++ assets/src/vite.config.js
@@ -1,72 +1,109 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import viteImagemin from 'vite-plugin-imagemin';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { resolve } from 'path';
+
+const externalDeps = {
+    '@woocommerce/blocks-registry'  : 'window.wc.wcBlocksRegistry',
+	'@woocommerce/settings'       	: 'window.wc.wcSettings',
+	'@woocommerce/blocks-checkout'  : 'window.wc.blocksCheckout',
+	'@wordpress/element'            : 'wp.element',
+	'@wordpress/i18n'               : 'wp.i18n',
+	'@wordpress/html-entities'      : 'wp.htmlEntities',
+	'@wordpress/blocks'             : 'wp.blocks',
+	'@wordpress/block-editor'       : 'wp.blockEditor',
+	'jquery'                        : 'jQuery',
+	'wp'                            : 'wp'
+}
+
+// Lista de entradas para compilar
+const entries = {
+    'js/frontend.min':  './js/interface/frontend.js',
+    'js/backend.min':   './js/interface/backend.js',
+    'js/admin.min':     './js/interface/admin.js',
+    'js/blocks.min':    './js/blocks/woocommerce/index.jsx',
+    'css/frontend.min': './scss/frontend.scss',
+    'css/backend.min':  './scss/backend.scss',
+    'css/admin.min':    './scss/admin.scss'
+};
 
 export default defineConfig(({ command, mode }) => {
     const isProduction = mode === 'production';
     const isDevelopment = mode === 'development';
+    const isBuild = command === 'build';
 
     return {
-        base: '/',
+        base: './',
         build: {
             outDir: '../dist',
             emptyOutDir: false,
             minify: isProduction,
             sourcemap: isDevelopment,
+            watch: isDevelopment && isBuild ? {} : null,
             rollupOptions: {
-                input: {
-                    'js/frontend.min': './js/interface/frontend.js',
-                    'js/backend.min': './js/interface/backend.js',
-                    'js/admin.min': './js/interface/admin.js',
-                    'css/frontend.min': './scss/frontend.scss',
-                    'css/backend.min': './scss/backend.scss',
-                    'css/admin.min': './scss/admin.scss'
-                },
+                input: entries,
+                external: Object.keys(externalDeps),
                 output: {
+                    format: 'es',
+                    globals: externalDeps,
                     entryFileNames: '[name].js',
                     assetFileNames: '[name][extname]'
                 }
             }
         },
-
-        server: {
-            host: 'wordpress.sandbox.local',
-            https: {
-                key: './certificates/wordpress.sandbox.local+3-key.pem',
-                cert: './certificates/wordpress.sandbox.local+3.pem'
-            },
-            watch: {
-                include: [
-                    './js/**/*.{js,jsx}',
-                    './scss/**/*.scss',
-                    '../../../**/*.php'
-                ],
-                exclude: [
-                    'node_modules/**',
-                    'dist/**',
-                    '**/.git/**'
-                ]
-            },
-            proxy: {
-                '^(?!/(@vite|node_modules|src))': {
-                    target: 'https://wordpress.sandbox.local',
-                    secure: false,
-                    changeOrigin: true
+        ...(isDevelopment && !isBuild ? {
+            server: {
+                host: 'wordpress.sandbox.local',
+                https: {
+                    key: './certificates/wordpress.sandbox.local+3-key.pem',
+                    cert: './certificates/wordpress.sandbox.local+3.pem'
+                },
+                watch: {
+                    include: ['./js/**/*.{js,jsx}', './scss/**/*.scss'],
+                    exclude: ['node_modules/**', 'dist/**']
+                },
+                proxy: {
+                    '^/': {
+                        target: 'https://wordpress.sandbox.local',
+                        changeOrigin: true,
+                        secure: false
+                    }
+                },
+                cors: true,
+                hmr: {
+                    protocol: 'wss',
+                    host: 'wordpress.sandbox.local',
+                    clientPort: 5173
                 }
             },
-            cors: true,
-            hmr: {
-                protocol: 'wss',
-                host: 'wordpress.sandbox.local',
-                clientPort: 5173
-            }
-        },
-
+        } : {}),
         plugins: [
-
+            react(),
+            {
+                name: 'wrap-in-iife',
+                generateBundle(options, bundle) {
+                    Object.keys(bundle).forEach(fileName => {
+                        const file = bundle[fileName];
+                        if (file.type === 'chunk' && fileName.endsWith('.js')) {
+                            file.code = `(function(){${file.code}})();`;
+                        }
+                    });
+                }
+            },
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: 'static/**/*',
+                        dest: '../dist'
+                    },
+                    {
+                        src: 'images/**/*',
+                        dest: '../dist/images'
+                    }
+                ]
+            }),
             viteImagemin({
-                disable: !isProduction,
+                disable: isDevelopment,
                 gifsicle: {
                     optimizationLevel: 3
                 },
@@ -83,18 +120,6 @@ export default defineConfig(({ command, mode }) => {
                         { name: 'removeEmptyAttrs', active: false }
                     ]
                 }
-            }),
-            viteStaticCopy({
-                targets: [
-                    {
-                        src: 'js/libs/**/*',
-                        dest: '../dist/js/libs'
-                    },
-                    {
-                        src: ['fonts/*', 'php/*'],
-                        dest: '../dist'
-                    }
-                ]
             })
         ],
 
