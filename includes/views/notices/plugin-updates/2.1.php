abed4df452b190e810b6cc4641285d209b0dd0ca
Henrique Cruz <eu@henriquecruz.com.br>
2026-01-22 19:32:17 -0300

    Better class handling for 2.1 update

--- includes/views/notices/plugin-updates/2.1.php
+++ includes/views/notices/plugin-updates/2.1.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php
 
 echo sprintf(
-	'<div class="error notice paghiper-dismiss-notice is-dismissible" data-notice-id="notice_2_1"><p><strong>%s: </strong>%s <a href="%s">%s</a></p></div>', 
+	'<div class="success updated paghiper-dismiss-notice is-dismissible" data-notice-id="notice_2_1"><p><strong>%s: </strong>%s <a href="%s">%s</a></p></div>', 
 	esc_html__('PIX PagHiper', 'woo-boleto-paghiper'), 
 	esc_html__('Você ja pode receber pagamentos por PIX! Configure aqui:', 'woo-boleto-paghiper'), 
 	esc_url(admin_url('admin.php?page=wc-settings&tab=checkout&section=wc_paghiper_pix_gateway')), 
